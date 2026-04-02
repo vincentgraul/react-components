dfd803f92976e94d5177160d2726027238fe1773
Vincent Graul <vincent.graul@outlook.com>
2026-04-02 22:04:24 +0200

    chore: update version

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  setupFiles: ["<rootDir>/tests/setup.js"],
+	preset: "ts-jest",
+	testEnvironment: "jsdom",
+	setupFiles: ["<rootDir>/tests/setup.js"],
 };
--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,2 @@
 declare module "*.css";
 declare module "*.svg";
-declare module "*.svg?react";
