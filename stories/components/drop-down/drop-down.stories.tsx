6c4527b6004cb512d037a0b55a9006b40a0d026f
Vincent Graul <vincent.graul@outlook.com>
2026-04-05 19:03:10 +0200

    test: add drop-down tests

--- stories/components/drop-down/drop-down.stories.tsx
+++ stories/components/drop-down/drop-down.stories.tsx
@@ -1,16 +1,33 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
+import { expect } from "storybook/test";
 import { DropDown } from "../../../src";
 
 const meta = {
 	component: DropDown,
+	args: {
+		children: <span>hello</span>,
+		label: "Menu",
+	},
 } satisfies Meta<typeof DropDown>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Basic: Story = {
-	args: {
-		children: <span>hello</span>,
-		label: "Menu",
+	play: async ({ canvas, userEvent }) => {
+		const button = canvas.getByRole("button");
+		await expect(canvas.queryByText("hello")).not.toBeInTheDocument();
+
+		// test open
+		await userEvent.click(button);
+		await expect(canvas.queryByText("hello")).toBeInTheDocument();
+
+		// test close
+		await userEvent.click(button);
+		await expect(canvas.queryByText("hello")).not.toBeInTheDocument();
+
+		//test re-open
+		await userEvent.click(button);
+		await expect(canvas.queryByText("hello")).toBeInTheDocument();
 	},
 };
