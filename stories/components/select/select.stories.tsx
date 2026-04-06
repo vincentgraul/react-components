02c2bf39f4c1487d3cafa56626078b7250fa56af
Vincent Graul <vincent.graul@outlook.com>
2026-04-06 15:55:33 +0200

    test: add select tests

--- stories/components/select/select.stories.tsx
+++ stories/components/select/select.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import { Bike, Car } from "lucide-react";
+import { expect, fn } from "storybook/test";
 import { Flex, Select } from "../../../src";
 
 const meta = {
@@ -11,17 +12,46 @@ const meta = {
 			{ label: "Italy", value: "it" },
 			{ label: "Scotland", value: "sc" },
 		],
+		onChange: fn(),
 	},
 } satisfies Meta<typeof Select>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Basic: Story = {};
+export const Basic: Story = {
+	play: async ({ args, canvas, userEvent }) => {
+		// Initial state
+		await expect(canvas.queryByRole("list")).not.toBeInTheDocument();
+		await expect(canvas.getByRole("button", { name: "Choose an option" })).toHaveTextContent(
+			"France",
+		);
+
+		// Show options
+		await userEvent.click(canvas.getByRole("button", { name: "Choose an option" }));
+
+		await expect(canvas.queryByRole("list")).toBeInTheDocument();
+
+		// Select an option
+		await userEvent.click(canvas.getByRole("button", { name: "Italy" }));
+
+		await expect(args.onChange).toHaveBeenCalledWith({ id: 2, label: "Italy", value: "it" });
+		await expect(canvas.queryByRole("list")).not.toBeInTheDocument();
+		await expect(canvas.getByRole("button", { name: "Choose an option" })).toHaveTextContent(
+			"Italy",
+		);
+		await expect(canvas.queryByRole("button", { name: "France" })).not.toBeInTheDocument();
+	},
+};
 
 export const WithSelectedOption: Story = {
 	args: {
 		selectedValue: "it",
+	},
+	play: async ({ canvas }) => {
+		await expect(canvas.getByRole("button", { name: "Choose an option" })).toHaveTextContent(
+			"Italy",
+		);
 	},
 };
 
@@ -54,10 +84,19 @@ export const WithOneOption: Story = {
 	args: {
 		options: [{ label: "Paris", value: "paris" }],
 	},
+	play: async ({ canvas }) => {
+		await expect(canvas.getByRole("button", { name: "Choose an option" })).toHaveStyle({
+			cursor: "default",
+			pointerEvents: "none",
+		});
+	},
 };
 
 export const WithLabel: Story = {
 	args: {
 		label: "Personnages",
+	},
+	play: async ({ canvas }) => {
+		await expect(canvas.queryByText("Personnages")).toBeInTheDocument();
 	},
 };
